556d10a377aee759d047c257594d5c3d143fe2db
BlueRaja <BlueRaja.admin@gmail.com>
2025-09-05 23:53:43 -0700

    Added 'Double Shards' mod

---
+++ double-shards/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace double_shards
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Hollow Knight Silksong.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPatch(typeof(PlayerData), "AddShards")]
+        [HarmonyPrefix]
+        private static void AddShardsPrefix(PlayerData __instance, ref int amount)
+        {
+            amount *= 2;
+        }
+    }
+}
